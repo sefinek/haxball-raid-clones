283a3d52f321d5a489caf79f2fafc336a10e3423
Sefinek <contact@sefinek.net>
2024-12-30 23:38:57 +0100

    http://

--- scripts/loadProxies.js
+++ scripts/loadProxies.js
@@ -4,7 +4,7 @@ module.exports = async path => {
 	try {
 		await fs.access(path);
 	} catch {
-		const defaultContent = '# Paste the SOCKS5 proxy here.\n# Example: socks5://LOGIN:PASSWORD@HOSTNAME:PORT\n';
+		const defaultContent = '# Paste the SOCKS5 proxy here.\n# Example: http://LOGIN:PASSWORD@HOSTNAME:PORT\n';
 		await fs.writeFile(path, defaultContent, 'utf8');
 		console.log(`Created ${path}. Please add your proxy.`);
 		process.exit(2);
