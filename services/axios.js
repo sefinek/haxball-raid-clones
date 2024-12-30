7145ae07c02f7e857e3a810f6f1aab0c559b0158
Sefinek <contact@sefinek.net>
2024-12-30 23:38:30 +0100

    Update headers

--- services/axios.js
+++ services/axios.js
@@ -3,18 +3,10 @@ const { name, version, homepage } = require('../package.json');
 
 axios.defaults.headers.common = {
 	'User-Agent': `Mozilla/5.0 (compatible; ${name}/${version}; +${homepage})`,
-	'Accept': 'application/json;v=b3;q=0.7',
-	'Accept-Encoding': 'gzip, deflate, br',
-	'Accept-Language': 'en;q=0.9',
-	'Cache-Control': 'max-age=0',
+	'Accept': 'application/json',
+	'Content-Type': 'application/json',
+	'Cache-Control': 'no-cache',
 	'Connection': 'keep-alive',
-	'Upgrade-Insecure-Requests': '1',
-	'Sec-Fetch-Dest': 'document',
-	'Sec-Fetch-Mode': 'navigate',
-	'Sec-Fetch-Site': 'same-origin',
-	'Sec-Fetch-User': '?1',
-	'Sec-CH-UA-Mobile': '?0',
-	'Priority': 'u=0, i',
 };
 
 module.exports = axios;
